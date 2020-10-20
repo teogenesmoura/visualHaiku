00faec7458a0dfa57e9f64ddb14aa9eec99b2cd6
teo <teo@teos-MacBook-Pro.local>
2020-10-19 22:08:55 -0300

    da pra ver o texto ihull

--- src/components/haikai/index.js
+++ src/components/haikai/index.js
@@ -18,19 +18,27 @@ const useStyles = makeStyles((theme) => ({
     height: '25vh',
     width: '100%',
     margin: '0 0 2rem 0',
-    backgroundColor: 'purple'
   },
   typography: {
     color: 'white',
     fontSize: 'x-large',
-    fontFamily: 'Open Sans'
-  }
+    fontFamily: 'Open Sans',
+    alignSelf: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    zIndex: '2',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
 }));
 
 export default function Haikai(props){
   const classes = useStyles();
   const [data, setData] = useState({...props.data});
   const [images, setImages] = useState('');
+  const [selectedNumbers, setSelectedNumbers] = useState([]);
 
   useEffect(() => {
     setData(props.data);
@@ -43,32 +51,43 @@ export default function Haikai(props){
     return t_images;
   }
 
-  function selectRandomImage() {
+  function randomNumber() {
     let min = Math.ceil(1);
     let max = Math.floor(Object.keys(images).length);
     let selected = Math.floor(Math.random() * (max - min)) + min;
-    return selected + '.jpg';
+    return selected;
   }
+
+  function selectRandomImage() {
+    return randomNumber() + '.jpg';
+  }
+
 	return (
     <Grid container className={classes.body}>
       <Grid item
             className={classes.sentenceContainer}
-            style={{backgroundImage: `url(${images[selectRandomImage()]})`,
-                    backgroundSize: '900px'}}
-            >
-        <Typography className={classes.typography}>{data.first_sentence} </Typography>
+            style={{
+                    backgroundImage: `url(${images[selectRandomImage()]})`,
+                    backgroundSize: '900px'}}>
+                    <div className={classes.overlay}>
+                      <Typography className={classes.typography}>{data.first_sentence} </Typography>
+                    </div>
       </Grid>
       <Grid item
             className={classes.sentenceContainer}
             style={{backgroundImage: `url(${images[selectRandomImage()]})`,
                     backgroundSize: '900px'}}>
-        <Typography className={classes.typography}>{data.second_sentence} </Typography>
+            <div className={classes.overlay}>
+              <Typography className={classes.typography}>{data.second_sentence} </Typography>
+            </div>
       </Grid>
       <Grid item
             className={classes.sentenceContainer}
             style={{backgroundImage: `url(${images[selectRandomImage()]})`,
                     backgroundSize: '900px'}}>
-        <Typography className={classes.typography}>{data.third_sentence} </Typography>
+                    <div className={classes.overlay}>
+                      <Typography className={classes.typography}>{data.third_sentence} </Typography>
+                    </div>
       </Grid>
     </Grid>
 	)
