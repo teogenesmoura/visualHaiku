7b41c5cdc3a7cd50521f2258d6a75232bd948651
teo <teo@teos-MacBook-Pro.local>
2020-10-22 17:00:57 -0300

    Updates

---
+++ precache-manifest.8d9ac9060fe32cddffa0e62826e4ea0e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ac7f61dadc1f527ed984555653aa60",
+    "url": "/teogenesmoura/visualHaiku/index.html"
+  },
+  {
+    "revision": "aeab72024196d74c506a",
+    "url": "/teogenesmoura/visualHaiku/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d905dcce8451778ca2f8",
+    "url": "/teogenesmoura/visualHaiku/static/js/2.b78f6dcf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teogenesmoura/visualHaiku/static/js/2.b78f6dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeab72024196d74c506a",
+    "url": "/teogenesmoura/visualHaiku/static/js/main.8d0b23bd.chunk.js"
+  },
+  {
+    "revision": "87f546f4481bc6fe3f09",
+    "url": "/teogenesmoura/visualHaiku/static/js/runtime-main.50db37cd.js"
+  },
+  {
+    "revision": "34ce0bd2e84d687360451f79908fc3d3",
+    "url": "/teogenesmoura/visualHaiku/static/media/1.34ce0bd2.jpg"
+  },
+  {
+    "revision": "0c85543ad3d8beec8acd979d8b206dc9",
+    "url": "/teogenesmoura/visualHaiku/static/media/10.0c85543a.jpg"
+  },
+  {
+    "revision": "188fe35f94fd509858185f02274c30b8",
+    "url": "/teogenesmoura/visualHaiku/static/media/11.188fe35f.jpg"
+  },
+  {
+    "revision": "f48d19d0f0552e0302dc58813863bb81",
+    "url": "/teogenesmoura/visualHaiku/static/media/12.f48d19d0.jpg"
+  },
+  {
+    "revision": "9bad6dd8f2dac930dd9340bec9205dae",
+    "url": "/teogenesmoura/visualHaiku/static/media/13.9bad6dd8.jpg"
+  },
+  {
+    "revision": "5c2a2585c6d8a9b108c8562b50a1a828",
+    "url": "/teogenesmoura/visualHaiku/static/media/14.5c2a2585.jpg"
+  },
+  {
+    "revision": "4633509317de7a73373329c3961f7e80",
+    "url": "/teogenesmoura/visualHaiku/static/media/15.46335093.jpg"
+  },
+  {
+    "revision": "01e59bcd62b0cd51c55fb8bb50e6bc9f",
+    "url": "/teogenesmoura/visualHaiku/static/media/2.01e59bcd.jpg"
+  },
+  {
+    "revision": "6d92ec8a235b83ba31bfd0449e99b769",
+    "url": "/teogenesmoura/visualHaiku/static/media/3.6d92ec8a.jpg"
+  },
+  {
+    "revision": "7598ff0a1b29226f9ec31b941ea7cba3",
+    "url": "/teogenesmoura/visualHaiku/static/media/4.7598ff0a.jpg"
+  },
+  {
+    "revision": "706149ae4ff0143eb47797592de72567",
+    "url": "/teogenesmoura/visualHaiku/static/media/5.706149ae.jpg"
+  },
+  {
+    "revision": "c6ab4f4a09e9028422a94c277fe730c3",
+    "url": "/teogenesmoura/visualHaiku/static/media/6.c6ab4f4a.jpg"
+  },
+  {
+    "revision": "75fb95c0469124068bdf21cc3e4eaf27",
+    "url": "/teogenesmoura/visualHaiku/static/media/7.75fb95c0.jpg"
+  },
+  {
+    "revision": "35c67376dee9805bfe8f3ef40d6a8f8d",
+    "url": "/teogenesmoura/visualHaiku/static/media/8.35c67376.jpg"
+  },
+  {
+    "revision": "943099d6fadfa93fd6107f9807c260d0",
+    "url": "/teogenesmoura/visualHaiku/static/media/9.943099d6.jpg"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/teogenesmoura/visualHaiku/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teogenesmoura/visualHaiku/static/media/logo.5d5d9eef.svg"
+  }
+]);
